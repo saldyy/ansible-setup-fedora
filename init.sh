1d37d968bf6140160ac31be4c3bbe148a60ae25b
Phillip Nguyen <png9981@gmail.com>
2025-12-07 14:19:41 +0700

    feat: add ghostty

--- init.sh
+++ init.sh
@@ -86,30 +86,46 @@ install_neovim() {
 }
 
 install_zsh() {
-  # KEEP_ZSHRC="yes"
-  #
-  # sudo dnf5 install -y zsh
-  # sudo chsh -s $(which zsh)
-  # # Install Oh My Zsh
-  # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  #
-  # # Install zsh-autosuggestions/completions plugins
-  # git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH}/custom/plugins/zsh-autosuggestions"
-  # git clone https://github.com/zsh-users/zsh-completions "${ZSH}/custom/plugins/zsh-completions"
+  KEEP_ZSHRC="yes"
+  DRACULA_ZSH_THEME_DIR="${THEMES_DIR}/dracula-zsh"
+
+  sudo dnf5 install -y zsh
+  sudo chsh -s $(which zsh)
+  # Install Oh My Zsh
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  
+  # Install zsh-autosuggestions/completions plugins
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH}/custom/plugins/zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-completions "${ZSH}/custom/plugins/zsh-completions"
   
   # Copy Dracula theme
-  DRACULA_ZSH_THEME_DIR="${THEMES_DIR}/dracula-zsh"
   create_dir $THEMES_DIR
 
-  git clone https://github.com/dracula/zsh.git $THEMES_DIR/dracula-zsh
+  rm -rf $DRACULA_ZSH_THEME_DIR
+  git clone https://github.com/dracula/zsh.git $DRACULA_ZSH_THEME_DIR
   ln -fs $DRACULA_ZSH_THEME_DIR/dracula.zsh-theme "${ZSH}/themes"
 }
 
+install_ghostty() {
+  DRACULA_GHOSTTY_THEME_DIR="$THEMES_DIR/dracula-ghostty"
+  GHOSTTY_CONFIG_DIR="$HOME/.config/ghostty"
+
+  sudo dnf5 copr enable -y scottames/ghostty
+  sudo dnf5 install -y ghostty
+
+  # Install Dracula theme
+  create_dir $THEMES_DIR
+  create_dir "$GHOSTTY_CONFIG_DIR/themes"
+  rm -rf $DRACULA_GHOSTTY_THEME_DIR
+  git clone https://github.com/dracula/ghostty.git $DRACULA_GHOSTTY_THEME_DIR
+  ln -fs $DRACULA_GHOSTTY_THEME_DIR/dracula "$GHOSTTY_CONFIG_DIR/themes"
+}
 
 init() {
   # install_packages
   # install_neovim
-  install_zsh
+  # install_zsh
+  install_ghostty
 }
 
 
